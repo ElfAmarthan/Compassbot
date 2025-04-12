171199d1af7ea0546cc8408245a093eccfb79667
Amarthan <bregva21@gmail.com>
2025-04-12 18:25:37 +0400

    upd

--- bot.py
+++ bot.py
@@ -32,6 +32,11 @@ app = Flask(__name__, template_folder='templates')
 def home():
     return "Welcome to the homepage!"
 
+if __name__ == '__main__':
+    # Use the PORT environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 @app.route('/')
 def show_map():
     return render_template('index.html')  # Make sure map.html exists
